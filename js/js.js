c4b7aa67e40cc4c3159eba7486267ea9eabca98c
Heitor <heitor.a.ha@gmail.com>
2022-10-01 01:30:00 -0300

    novas funcionalidades para seção projetos

--- js/js.js
+++ js/js.js
@@ -28,10 +28,21 @@ function escrevendo (nome) {
 
 /* Função muda imagem dos projetos automaticamente */
 
+let bancoImagens = [
+    ['image/clinicamedica.png', 'Lading Page clínica Médica com Html, css criada durante curso'],
+    ['image/orcamentopessoal.png', 'Aplicação para controle de despesas com possibilidade de criar e excluir despesas'],
+    ['image/matainseto.png', 'Jogo divertido mata inseto com limite de vida e cronômetro para a vitória'],
+    ['image/cep.png', 'Aplicação ligada ao uma API que localizar endereço'],
+    ['image/sitenoticias.png', 'Página modelo para um site de notícia']
+]
+
 function mudaImageProj(e){
-    document.getElementById('imageProj').src = e;
-    document.getElementById('imageModal').src = e;
+    document.getElementById('imageProj').src = bancoImagens[e][0];
+    document.getElementById('imageModal').src = bancoImagens[e][0];
+    document.getElementById('legendaimage').innerHTML = bancoImagens[e][1];
+    document.getElementById('legandaImgModal').innerHTML = bancoImagens[e][1];
 }
+
 
 
 
