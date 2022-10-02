a3be5706f1662f590930dc611d19295fc9f4c7d9
Heitor <heitor.a.ha@gmail.com>
2022-10-02 06:52:27 -0300

    Corrigindo a Responsividade e aplicando efeitos scroll

--- js/js.js
+++ js/js.js
@@ -1,24 +1,39 @@
+/* Scroll */
+
+ScrollReveal().reveal('#topo', {duration: 5000});
+ScrollReveal().reveal('.banner-topo-apresentacao');
+
+/*Sobre*/
+ScrollReveal().reveal('#sobre', {rotate: {x: 70, y: 0, z: 0}, duration: 2500});
+
+/* Habilidades */
+ScrollReveal().reveal('.titulo-habilidades', { scale: 0.85, duration: 3000});
+ScrollReveal().reveal('#habilidades h1', {duration: 3000});
+ScrollReveal().reveal('.skills', {duration: 4000});
+
+/* Projetos */
+ScrollReveal().reveal('#projetos h1', {duration: 2000});
+ScrollReveal().reveal('.menu-projeto', {duration: 3000});
+ScrollReveal().reveal('.projetos-imagens', {duration: 4000});
+
+/* ---- Menu contato lateral */
+//ScrollReveal().reveal('.link-contato-lateral', {duration: 4000});
+
+/*seta subir*/
+ScrollReveal().reveal('#seta-subir', {duration: 4000});
+
+/* -------- Função escreve nome de apresentação -------- */
 const nomeh = document.getElementById('texto-apresentacao');
 const fname = nomeh.innerHTML;
 const imageProj = document.getElementById('image-projeto');
 
-window.onscroll = function(){
-    efeitoCarrega();
-}
-
-function efeitoCarrega(){
-    if(document.documentElement.scrollTop > 350){
-        document.getElementById('sobre').className = 'show d-flex';
-    }
-}
-
-/* Função escreve nome de apresentação */
+nomeh.innerHTML='';
 
 function escrevendo (nome) {
     setTimeout( () => {
-    const meuNome = nome.split('');
     nomeh.innerHTML='';
-    meuNome.forEach( (l, x) => {
+    const meuNome = nome.split('');
+    meuNome.forEach((l, x) => {
         setTimeout(function (){
             nomeh.innerHTML += l;
         }, 80 * x)
@@ -26,18 +41,26 @@ function escrevendo (nome) {
     
 }
 
-/* Função muda imagem dos projetos automaticamente */
+/* Menu contato collapse JQuery */
 
+$('#contato').click(function(){
+    $('.link-contato-superior').slideToggle(500);
+    $('.link-contato-lateral').slideToggle(500);
+})
+
+
+
+/* Função muda imagem dos projetos automaticamente */
 /* Array criado para armazenar informações da cada projeto individualmente.
     Cada imagem tem um número que está ligado a primera informação(id) do array.
 */
 
 let bancoImagens = [
     ['0', 'image/clinicamedica.png', 'Lading Page clínica Médica com Html, css criada durante curso'],
-    ['1','image/orcamentopessoal.png', 'Aplicação para controle de despesas com possibilidade de criar e excluir despesas'],
+    ['1','image/orcamentopessoal.png', 'Aplicação para controle de despesas, podendo criar e excluir despesas'],
     ['2','image/matainseto.png', 'Jogo divertido mata inseto com limite de vida e cronômetro para a vitória'],
     ['3','image/cep.png', 'Aplicação ligada ao uma API que localizar endereço'],
-    ['4','image/sitenoticias.png', 'Página modelo para um site de notícia']
+    ['4','image/sitenoticias.png', 'Página modelo para um site de notícia com Bootstrap']
 ]
 
 function mudaImageProj(e){
