a7846c61a66d63d04b71a85912fa95933c34b9e2
Heitor <heitor.a.ha@gmail.com>
2022-10-02 01:56:42 -0300

    Adicionando (id) ni array para identificação das imagens do projeto

--- js/js.js
+++ js/js.js
@@ -28,19 +28,23 @@ function escrevendo (nome) {
 
 /* Função muda imagem dos projetos automaticamente */
 
+/* Array criado para armazenar informações da cada projeto individualmente.
+    Cada imagem tem um número que está ligado a primera informação(id) do array.
+*/
+
 let bancoImagens = [
-    ['image/clinicamedica.png', 'Lading Page clínica Médica com Html, css criada durante curso'],
-    ['image/orcamentopessoal.png', 'Aplicação para controle de despesas com possibilidade de criar e excluir despesas'],
-    ['image/matainseto.png', 'Jogo divertido mata inseto com limite de vida e cronômetro para a vitória'],
-    ['image/cep.png', 'Aplicação ligada ao uma API que localizar endereço'],
-    ['image/sitenoticias.png', 'Página modelo para um site de notícia']
+    ['0', 'image/clinicamedica.png', 'Lading Page clínica Médica com Html, css criada durante curso'],
+    ['1','image/orcamentopessoal.png', 'Aplicação para controle de despesas com possibilidade de criar e excluir despesas'],
+    ['2','image/matainseto.png', 'Jogo divertido mata inseto com limite de vida e cronômetro para a vitória'],
+    ['3','image/cep.png', 'Aplicação ligada ao uma API que localizar endereço'],
+    ['4','image/sitenoticias.png', 'Página modelo para um site de notícia']
 ]
 
 function mudaImageProj(e){
-    document.getElementById('imageProj').src = bancoImagens[e][0];
-    document.getElementById('imageModal').src = bancoImagens[e][0];
-    document.getElementById('legendaimage').innerHTML = bancoImagens[e][1];
-    document.getElementById('legandaImgModal').innerHTML = bancoImagens[e][1];
+    document.getElementById('imageProj').src = bancoImagens[e][1];
+    document.getElementById('imageModal').src = bancoImagens[e][1];
+    document.getElementById('legendaimage').innerHTML = bancoImagens[e][2];
+    document.getElementById('legandaImgModal').innerHTML = bancoImagens[e][2];
 }
 
 
